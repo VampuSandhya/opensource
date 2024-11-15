d064e1d3d38f18a449a2c9beceb7c988378b0125
VampuSandhya <142558556+VampuSandhya@users.noreply.github.com>
2024-11-15 11:31:54 +0530

    Create Count_Notebooks.java

---
+++ visaa_prep/Count_Notebooks.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int books = N * 10;
+        System.out.println(books);
+    }
+}
